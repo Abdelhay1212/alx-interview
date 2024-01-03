5f19e4c501967153aca1adc79a033dab7daca5b9
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-03 18:04:52 +0000

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(n - 1):
+        temp = [0] + triangle[i] + [0]
+        row = []
+
+        for j in range(len(triangle[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+
+        triangle.append(row)
+
+    return triangle

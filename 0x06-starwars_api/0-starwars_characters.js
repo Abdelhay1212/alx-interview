a3a78dc592d6018177ec414261d81cd8ad9d0ce1
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-14 19:00:20 +0000

    updatedù

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,25 +2,34 @@
 /* Star Wars Characters */
 const request = require('request');
 
+const fetch = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+};
+
+const fetchCharacter = (url) => {
+  return fetch(url).then(data => data.name);
+};
+
+const fetchFilmCharacters = (id) => {
+  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+  fetch(url).then(data => {
+    const CharactersPromises = data.characters.map(url => fetchCharacter(url));
+    return Promise.all(CharactersPromises);
+  }).then(names => {
+    names.forEach(name => console.log(name));
+  }).catch(error => console.error(error));
+};
+
 const args = process.argv.slice(2);
 const id = args[0];
 
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const characters = data.characters;
-    for (const person of characters) {
-      request(person, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        } else {
-          console.log(error);
-        }
-      });
-    }
-  } else {
-    console.error(error);
-  }
-});
+fetchFilmCharacters(id);

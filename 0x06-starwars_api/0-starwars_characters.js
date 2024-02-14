c75c4c0158fa09774e0a8572c6e1f0eb2d515773
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-14 17:54:19 +0000

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* Star Wars Characters */
+const request = require('request');
+
+args = process.argv.slice(2);
+const id = args[0];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    for (let person of characters) {
+      request(person, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          const data = JSON.parse(body)
+          console.log(data.name);
+        } else {
+          console.log(error);
+        }
+      });
+    }
+  } else {
+    console.error(error);
+  }
+});

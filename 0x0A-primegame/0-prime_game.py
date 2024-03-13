a82c5ef924a581a0258e67be5067fa3fc41ebc6c
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-13 11:13:32 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,24 +4,27 @@
 
 def isWinner(x, nums):
     '''return the winner or None'''
-    def SieveOfEratosthenes(n):
+    def countPrimes(n):
+        ''''''
         if n < 2:
             return False
         p = 2
         prime = [True] * (n+1)
+        prime[0] = prime[1] = False
         while p * p <= n:
             if prime[p] == True:
                 for i in range(p * p, n+1, p):
                     prime[i] = False
             p += 1
-        return sum(prime[2:]) % 2 == 1
+        return prime.count(True)
 
     ben, maria = 0, 0
     for num in nums:
-        if SieveOfEratosthenes(num):
-            maria += 1
-        else:
+        count = countPrimes(num)
+        if count % 2 == 0:
             ben += 1
+        else:
+            maria += 1
 
     if ben > maria:
         return 'Ben'
@@ -29,7 +32,3 @@ def isWinner(x, nums):
         return 'Maria'
     else:
         return None
-
-
-print("Winner: {}".format(isWinner(3, [4, 5, 1])))
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
